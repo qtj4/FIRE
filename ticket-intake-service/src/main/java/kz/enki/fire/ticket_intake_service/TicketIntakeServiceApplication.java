972cebccb26a9cf799aad37b3a4d5227fbb97113
tima <temirlanzholaev@gmail.com>
2026-02-21 16:29:26 +0500

    feat: inizialize ticket intake service

---
+++ ticket-intake-service/src/main/java/kz/enki/fire/ticket_intake_service/TicketIntakeServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.enki.fire.ticket_intake_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketIntakeServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketIntakeServiceApplication.class, args);
+	}
+
+}
---
+++ ticket-intake-service/src/test/java/kz/enki/fire/ticket_intake_service/TicketIntakeServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.enki.fire.ticket_intake_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketIntakeServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
