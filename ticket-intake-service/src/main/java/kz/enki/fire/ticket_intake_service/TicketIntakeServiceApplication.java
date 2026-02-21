2f38d264f340f60714d48c772eccc9c3a6cbf4d6
tima <temirlanzholaev@gmail.com>
2026-02-21 19:19:55 +0500

    feat: parse csv to object and save to db

--- ticket-intake-service/src/main/java/kz/enki/fire/ticket_intake_service/TicketIntakeServiceApplication.java
+++ ticket-intake-service/src/main/java/kz/enki/fire/ticket_intake_service/TicketIntakeServiceApplication.java
@@ -1,13 +1,32 @@
 package kz.enki.fire.ticket_intake_service;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Enumeration;
 
 @SpringBootApplication
 public class TicketIntakeServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TicketIntakeServiceApplication.class, args);
+	}
+
+	@PostConstruct
+	public void debugClasspath() {
+		System.out.println("--- CLASSPATH DEBUG ---");
+		try {
+			Enumeration<URL> resources = getClass().getClassLoader().getResources("db/changelog/");
+			while (resources.hasMoreElements()) {
+				System.out.println("Found resource: " + resources.nextElement());
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+		System.out.println("-----------------------");
 	}
 
 }
