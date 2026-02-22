9cd656a20fd0a635eb866d4acc69a83ebd4439bf
aidyn <a.nurlan@documentolog.com>
2026-02-22 13:05:57 +0500

    scalability

--- evaluation-service/src/main/java/kz/enki/fire/evaluation_service/controller/IntakeController.java
+++ evaluation-service/src/main/java/kz/enki/fire/evaluation_service/controller/IntakeController.java
@@ -21,7 +21,7 @@ import java.util.List;
 import java.util.UUID;
 
 @RestController
-@RequestMapping({"/api/v1/intake"})
+@RequestMapping({"/api/v1/intake", "/api/evaluation/intake"})
 @RequiredArgsConstructor
 public class IntakeController {
 
